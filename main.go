aff854bdd97ccc13664d23bb8f52705edb534703
elio <rruinmi1@gmail.com>
2025-10-13 22:20:22 +0800

    finish

--- main.go
+++ main.go
@@ -2,50 +2,58 @@ package main
 
 import (
 	"context"
-	"os"
 	"fmt"
-	"github.com/fatih/color"
+	"os"
 
+	"github.com/fatih/color"
 	"github.com/go-faster/errors"
 	"github.com/gotd/td/tg"
 	"github.com/iyear/tdl/extension"
-	"github.com/neilotoole/jsoncolor"
 	"github.com/mattn/go-colorable"
+	"github.com/neilotoole/jsoncolor"
 	"github.com/spf13/pflag"
 )
 
-var firstName string
-var lastName string
-var about string
+type profileUpdate struct {
+	FirstName *string
+	LastName  *string
+	About     *string
+}
 
-func init() {
-	pflag.StringVarP(&firstName, "first-name", "f", "!@#^%$@#$&$&*&@)*(_).,;oyfgdasvbn", "新的名字")
-	pflag.StringVarP(&lastName, "last-name", "l", "!@#^%$@#$&$&*&@)*(_).,;oyfgdasvbn", "新的姓氏")
-	pflag.StringVarP(&about, "about", "a", "!@#^%$@#$&$&*&@)*(_).,;oyfgdasvbn", "新的自我介绍")
-	pflag.Parse()
+func (u profileUpdate) Empty() bool {
+	return u.FirstName == nil && u.LastName == nil && u.About == nil
 }
 
 func main() {
+	update, showHelp, err := parseFlags(os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	if showHelp {
+		return
+	}
+
 	extension.New(extension.Options{
 		UpdateHandler: nil,
 		Middlewares:   nil,
-		Logger: nil,
+		Logger:        nil,
 	})(func(ctx context.Context, e *extension.Extension) error {
-		if firstName == "" && lastName == "" && about == "" {
+		if update.Empty() {
 			output("没有提供任何更新内容", nil, "")
 			return nil
 		}
 
 		api := e.Client().API()
 		req := &tg.AccountUpdateProfileRequest{}
-		if firstName != "!@#^%$@#$&$&*&@)*(_).,;oyfgdasvbn" {
-			req.SetFirstName(firstName)
+		if update.FirstName != nil {
+			req.SetFirstName(*update.FirstName)
 		}
-		if lastName != "!@#^%$@#$&$&*&@)*(_).,;oyfgdasvbn" {
-			req.SetLastName(lastName)
+		if update.LastName != nil {
+			req.SetLastName(*update.LastName)
 		}
-		if about != "!@#^%$@#$&$&*&@)*(_).,;oyfgdasvbn" {
-			req.SetAbout(about)
+		if update.About != nil {
+			req.SetAbout(*update.About)
 		}
 		_, err := api.AccountUpdateProfile(ctx, req)
 		if err != nil {
@@ -55,6 +63,45 @@ func main() {
 	})
 }
 
+func parseFlags(args []string) (profileUpdate, bool, error) {
+	var update profileUpdate
+
+	var (
+		firstName string
+		lastName  string
+		about     string
+	)
+
+	flagSet := pflag.NewFlagSet("tdl-profile", pflag.ContinueOnError)
+	writer := colorable.NewColorable(os.Stdout)
+	flagSet.SetOutput(writer)
+	flagSet.StringVarP(&firstName, "first-name", "f", "", "新的名字")
+	flagSet.StringVarP(&lastName, "last-name", "l", "", "新的姓氏")
+	flagSet.StringVarP(&about, "about", "a", "", "新的自我介绍")
+	flagSet.Usage = func() {
+		fmt.Fprintln(writer, "Usage of tdl-profile:")
+		flagSet.PrintDefaults()
+	}
+
+	if err := flagSet.Parse(args); err != nil {
+		if err == pflag.ErrHelp {
+			return update, true, nil
+		}
+		return update, false, err
+	}
+
+	if flagSet.Lookup("first-name").Changed {
+		update.FirstName = &firstName
+	}
+	if flagSet.Lookup("last-name").Changed {
+		update.LastName = &lastName
+	}
+	if flagSet.Lookup("about").Changed {
+		update.About = &about
+	}
+
+	return update, false, nil
+}
 
 func output(header string, v any, footer string) {
 	if header != "" {
