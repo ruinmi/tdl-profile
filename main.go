0c07757e034ca002617ce24a13707d3627817e91
elio <rruinmi1@gmail.com>
2025-10-13 20:16:36 +0800

    test1

--- main.go
+++ main.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
 	"github.com/go-faster/errors"
 	"github.com/iyear/tdl/extension"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-	conf := zap.NewDevelopmentConfig()
-	conf.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-
 	extension.New(extension.Options{
 		UpdateHandler: nil,
 		Middlewares:   nil,
 		Logger: nil,
 	})(func(ctx context.Context, e *extension.Extension) error {
-		self, err := e.Client().Self(ctx)
-		if err != nil {
-			return errors.Wrap(err, "get self")
-		}
 
-		api := client.API()
+		api := e.Client().API()
 		req := &tg.AccountUpdateProfileRequest{}
 		req.SetAbout("我的新自我介绍 ✨")
 		_, err := api.AccountUpdateProfile(ctx, req)
